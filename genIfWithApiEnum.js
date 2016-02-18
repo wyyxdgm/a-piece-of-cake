365a4b9af00f28bc48647321e11ef18c9848456a
guimaodai <wyyxdgm@163.com>
2016-02-18 18:05:33 +0800

    util

--- genIfWithApiEnum.js
+++ genIfWithApiEnum.js
@@ -4,7 +4,26 @@ if (!fs.existsSync(file)) {
     return console.log("no file==> " + file);
 }
 var text = fs.readFileSync(file, "utf-8");
-var reg = /^(\s*)(\S*)(\s*)([^:]*):|:(\S*)(\s*)$/g;
-var result = reg.exec(text);
-console.log(result);
 console.log(text);
+var result;
+var reg = /(\s*)([\/\d]*\.)(\s*)([^:\s]*)(:|：)\s*(\S+)(\s*)/g;
+var codeReg = /('|")([^'"]+)\1/g;
+var obj = {};
+
+while (result = reg.exec(text)) {
+    console.log(result);
+    var re;
+    var val = result[6];
+    var key = result[4].replace(/['"]/g, "");
+    obj[key] = val;
+}
+
+console.log(obj);
+
+for (var key in obj) {
+    console.log(key + "\t" + obj[key], isNaN(parseFloat(key)));
+}
+
+
+// console.log(result);
+// console.log(text);
